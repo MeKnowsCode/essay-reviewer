ae9aef47f6d85edfd8ffab3009e60f84e49a16bb
MeKnowsCode <faidarik@mail.ru>
2026-04-02 21:37:16 -0400

    Create proxy.js

---
+++ netlify/functions/proxy.js
@@ -0,0 +1,37 @@
+exports.handler = async function(event) {
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, x-api-key, anthropic-version'
+      },
+      body: ''
+    };
+  }
+
+  const body = JSON.parse(event.body);
+  const apiKey = event.headers['x-api-key'];
+
+  const response = await fetch('https://api.anthropic.com/v1/messages', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-api-key': apiKey,
+      'anthropic-version': '2023-06-01'
+    },
+    body: JSON.stringify(body)
+  });
+
+  const data = await response.json();
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify(data)
+  };
+};
